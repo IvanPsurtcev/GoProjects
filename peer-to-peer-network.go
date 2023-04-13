b1a2a91750c7aaf8d47108d7b25f602a5bc69977
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2023-04-13 23:02:28 +0300

    Create peer-to-peer-network.go

---
+++ peer-to-peer-network.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+type Node struct {
+	Connections map[string]bool
+	Address     Address
+}
+
+type Address struct {
+	IPv4 string
+	Port string
+}
+
+type Package struct {
+	To   string
+	From string
+	Data string
+}
+
+// ./main :8080
+func init() {
+	if len(os.Args) != 2 {
+		panic("len args != 2")
+	}
+}
+
+func main() {
+	NewNode(os.Args[1]).Run(handleServer, handleClient)
+}
+
+// ipv4:port
+// 127.0.0.1:8080
+func NewNode(address string) *Node {
+	splited := strings.Split(address, ":")
+	if len(splited) != 2 {
+		return nil
+	}
+	return &Node{
+		Connections: make(map[string]bool),
+		Address: Address{
+			IPv4: splited[0],
+			Port: ":" + splited[1],
+		},
+	}
+}
+
+func (node *Node) Run(handleServer func(*Node), handleClient func(*Node)) {
+	go handleServer(node)
+	handleClient(node)
+}
+
+func handleServer(node *Node) {
+	listen, err := net.Listen("tcp", "0.0.0.0"+node.Address.Port)
+	if err != nil {
+		panic("listen error")
+	}
+	defer listen.Close()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			break
+		}
+		go handleConnection(node, conn)
+	}
+}
+
+func handleConnection(node *Node, conn net.Conn) {
+	defer conn.Close()
+	var (
+		buffer  = make([]byte, 512)
+		message string
+		pack    Package
+	)
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil {
+			break
+		}
+		message += string(buffer[:length])
+	}
+	err := json.Unmarshal([]byte(message), &pack)
+	if err != nil {
+		return
+	}
+	node.ConnectTo([]string{pack.From})
+	fmt.Println(pack.Data)
+}
+
+func handleClient(node *Node) {
+	for {
+		message := InputString()
+		splited := strings.Split(message, " ")
+		switch splited[0] {
+		case "/exit":
+			os.Exit(0)
+		case "/connect":
+			node.ConnectTo(splited[1:])
+		case "/network":
+			node.PrintNetwork()
+		default:
+			node.SendMessageToAll(message)
+		}
+	}
+}
+
+func (node *Node) PrintNetwork() {
+	for addr := range node.Connections {
+		fmt.Println("|", addr)
+	}
+}
+
+func (node *Node) ConnectTo(addresses []string) {
+	for _, addr := range addresses {
+		node.Connections[addr] = true
+	}
+}
+
+func (node *Node) SendMessageToAll(message string) {
+	var newPack = &Package{
+		From: node.Address.IPv4 + node.Address.Port,
+		Data: message,
+	}
+	for addr := range node.Connections {
+		newPack.To = addr
+		node.Send(newPack)
+	}
+}
+
+func (node *Node) Send(pack *Package) {
+	conn, err := net.Dial("tcp", pack.To)
+	if err != nil {
+		delete(node.Connections, pack.To)
+		return
+	}
+	defer conn.Close()
+	jsonPack, _ := json.Marshal(*pack)
+	conn.Write(jsonPack)
+}
+
+func InputString() string {
+	msg, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	return strings.Replace(msg, "\n", "", 1)
+}
