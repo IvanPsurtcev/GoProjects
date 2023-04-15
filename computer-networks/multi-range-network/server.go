b5d94fd682ed102dc580b0063c7c5b49970a0b9b
IvanPsurtcev <ivanpsurcev@gmail.com>
2023-04-15 16:51:50 +0300

    add multi-range-network

---
+++ computer-networks/multi-range-network/server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"net"
+	"strings"
+)
+
+const (
+	END_BYTES = "\000\001\002"
+	PORT      = ":8080"
+)
+
+var (
+	Connections = make(map[net.Conn]bool)
+)
+
+func main() {
+	listen, err := net.Listen("tcp", PORT)
+	if err != nil {
+		panic("server error")
+	}
+	defer listen.Close()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			break
+		}
+		go handleConnect(conn)
+	}
+}
+
+func handleConnect(conn net.Conn) {
+	Connections[conn] = true
+	var (
+		buffer  = make([]byte, 512)
+		message string
+	)
+
+close:
+	for {
+		message = ""
+		for {
+			length, err := conn.Read(buffer)
+			if err != nil {
+				break close
+			}
+			message += string(buffer[:length])
+			if strings.HasSuffix(message, END_BYTES) {
+				message = strings.TrimSuffix(message, END_BYTES)
+				break
+			}
+		}
+		log.Println(message)
+		for c := range Connections {
+			if c == conn {
+				break
+			}
+			c.Write([]byte(strings.ToUpper(message) + END_BYTES))
+		}
+	}
+	delete(Connections, conn)
+}
